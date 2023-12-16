7d01fde2631e44b251e5dae99f84e7934f3ffc57
Nikita <nikita.mane2008@gmail.com>
2023-12-16 15:41:53 +0530

    code changes for add student

--- src/main/java/com/jjm/stdApp4/Service/StudentServiceImpl.java
+++ src/main/java/com/jjm/stdApp4/Service/StudentServiceImpl.java
@@ -1,4 +1,16 @@
 package com.jjm.stdApp4.Service;
 
-public class StudentServiceImpl {
+import com.jjm.stdApp4.entity.Student;
+import com.jjm.stdApp4.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    StudentRepository  studentRepository;
+    @Override
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
 }
--- src/main/java/com/jjm/stdApp4/repository/StudentRepository.java
+++ src/main/java/com/jjm/stdApp4/repository/StudentRepository.java
@@ -1,9 +1,9 @@
 package com.jjm.stdApp4.repository;
 
+import com.jjm.stdApp4.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
-
-
+public interface StudentRepository extends MongoRepository<Student,Integer> {
 }
